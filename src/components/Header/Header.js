77f2ee5823b8ab01a5d09912c5026d6e6c430f12
Michael <michaelr9wilson@gmail.com>
2022-02-02 18:08:35 -0800

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+
+const Header = () =>{
+    return (
+        <div className={styles.header}>
+            <div className={styles.headerTitle}>
+                Pokedex
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/MainContainer/MainContainer.js
@@ -0,0 +1,11 @@
+import styles from './MainContainer.module.css'
+
+const MainContainer = () =>{
+    return (
+        <div className={styles.mainContainer}>
+
+        </div>
+    )
+}
+
+export default MainContainer

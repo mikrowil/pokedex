566c1b6190d39d76a3dde9379c2bcaf6c53a659c
mikrowil <67307229+mikrowil@users.noreply.github.com>
2022-04-12 22:54:36 -0700

    updadted header and home (#3)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,73 +1,123 @@
 import styled from "styled-components";
-import {Link, Typography} from "@mui/material";
+import {Icon, Link, Typography, useTheme} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
-  padding: 0.5rem 1rem;
-  
+  padding: 2rem 1rem;
+
   align-items: center;
-  
-  border-bottom: 1px solid ${({theme})=> theme.palette.secondary.light};
+  justify-content: center;
 `
 
 const NavContainer = styled.div`
   display: flex;
   flex: 1;
-  
+
   align-items: center;
-  
+
   margin: 0 2rem;
 `
 
 const StyledLink = styled(Link)`
-  &&{
+
+  position: relative;
+
+  && {
     cursor: pointer;
-    
-    color: ${({theme})=> theme.palette.primary[300]};
-    
+
+    color: ${({theme}) => theme.palette.primary[300]};
+
     text-decoration: none;
     text-underline: none;
   }
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 2px;
+
+    background-color: currentColor;
+
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform 200ms ease-in;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
+`
+
+const AvatarContainer = styled.div`
+  box-shadow: 0 0 0 2px #ea1818;
+
+  height: 50px;
+  width: 50px;
+
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 50%;
+  
 `
 
 const navItems = [
-    {title:"pokemon"}
+    {title: "pokemon"}
 ]
 
 const Header = () => {
 
     const navigate = useNavigate()
+    const theme = useTheme()
 
     return (
         <Container>
-
             <div
-                style={{cursor:"pointer"}}
-                onClick={()=>{
-                    navigate('/')
+                style={{
+                    maxWidth: theme.breakpoints.values.lg,
+                    display: "flex",
+                    flex: 1,
+                    alignItems: "center"
                 }}
             >
-                <Typography
-                    variant={"h5"}
+                <div
+                    style={{cursor: "pointer"}}
+                    onClick={() => {
+                        navigate('/')
+                    }}
                 >
-                    Pokedex
-                </Typography>
+                    <Typography
+                        variant={"h5"}
+                    >
+                        Pokedex
+                    </Typography>
+                </div>
+
+                <NavContainer>
+                    {
+                        navItems.map(({title}) => (
+                            <StyledLink>
+                                {title}
+                            </StyledLink>
+                        ))
+                    }
+                </NavContainer>
+
+
+                <AvatarContainer>
+                    <Icon
+                        fontSize={"large"}
+                    >{"person"}</Icon>
+                </AvatarContainer>
+
             </div>
-
-            <NavContainer>
-                {
-                    navItems.map(({title})=>(
-                        <StyledLink>
-                            {title}
-                        </StyledLink>
-                    ))
-                }
-            </NavContainer>
-
-            <Typography>
-                Profile
-            </Typography>
         </Container>
     )
 }
